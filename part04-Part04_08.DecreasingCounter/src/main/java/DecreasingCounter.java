5cbdefbfb1ea4e2ebd1f5fcf17c4ee3d3e34e520
anrixs <anrijs.avins@gmail.com>
2025-08-09 18:24:00 +0300

    initial commit

--- part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
+++ part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
@@ -15,7 +15,18 @@ public class DecreasingCounter {
     public void decrement() {
         // write the method implementation here
         // the aim is to decrement the value of the counter by one
+        
+        if (this.value > 0) {
+            this.value--;
+        }
+        
     }
 
     // the other methods go here
+    
+    public void reset() {
+        if (this.value != 0) {
+            this.value = 0;
+        }
+    }
 }
