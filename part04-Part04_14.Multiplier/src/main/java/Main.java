df7f00f8e29293bf1ccba6c4ae44df3724ecf3d0
anrixs <anrijs.avins@gmail.com>
2025-08-10 18:22:24 +0300

    initial commit

--- part04-Part04_14.Multiplier/src/main/java/Main.java
+++ part04-Part04_14.Multiplier/src/main/java/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // Test your Multiplier class here
 
-        // Multiplier multiplyByThree = new Multiplier(3);
+        Multiplier multiplyByThree = new Multiplier(3); //changes the this.value to 3
 
-        // System.out.println("multiplyByThree.multiply(2): " + multiplyByThree.multiply(2));
+        System.out.println("multiplyByThree.multiply(2): " + multiplyByThree.multiply(2)); //multiplies this.value=3 * 2 = int number
 
-        // Multiplier multiplyByFour = new Multiplier(4);
+        Multiplier multiplyByFour = new Multiplier(4);
 
-        // System.out.println("multiplyByFour.multiply(2): " + multiplyByFour.multiply(2));
-        // System.out.println("multiplyByThree.multiply(1): " + multiplyByThree.multiply(1));
-        // System.out.println("multiplyByFour.multiply(1): " + multiplyByFour.multiply(1));
+        System.out.println("multiplyByFour.multiply(2): " + multiplyByFour.multiply(2));
+        System.out.println("multiplyByThree.multiply(1): " + multiplyByThree.multiply(1));
+        System.out.println("multiplyByFour.multiply(1): " + multiplyByFour.multiply(1));
     }
 }
