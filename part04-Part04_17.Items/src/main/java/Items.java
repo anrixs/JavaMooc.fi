3b6ea7633ee68ef346e07d0b6c1492ea27c7c685
anrixs <anrijs.avins@gmail.com>
2025-08-11 18:21:29 +0300

    initial commit

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -9,6 +9,20 @@ public class Items {
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            System.out.println("Name: ");
+            String input = scanner.nextLine();
+            
+            if (input.isEmpty()) {
+                break;
+            }
+            items.add(new Item(input));
+        }
+        System.out.println();
+        
+        for (Item item: items) {
+            System.out.println(item);
+        }
     }
 }
