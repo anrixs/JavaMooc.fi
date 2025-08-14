4d2e26b41817091db4cf86aa4fd9df79914684c0
anrixs <anrijs.avins@gmail.com>
2025-08-14 19:55:30 +0300

    initial commit

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,17 +1,39 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
+
 
 public class IsItInTheFile {
 
     public static void main(String[] args) {
+        boolean isFound = false;
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
-
+        
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        try (Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
 
+                if (searchedFor.equals(row)) {
+                    isFound = true;
+                    break;
+                }
+
+            }
+            if (isFound) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Reading the file " + ex + " failed.");
+        }     
     }
 }
