ca9354363e95d967ac72bd0e809f802d66712a80
anrixs <anrijs.avins@gmail.com>
2025-08-14 17:32:57 +0300

    initial commit

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,17 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String input = "";
+        int counter = 0;
+        while (true) {
+            input = scanner.nextLine();
+            counter++;
+            
+            if (input.equals("end")) {
+                counter--;
+                break;
+            } 
+        }
+        System.out.println(counter); // or (--counter);
     }
 }
