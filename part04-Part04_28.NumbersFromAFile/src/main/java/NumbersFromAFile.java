fb8f758018b854514d0b0247af2cef81c67474bf
anrixs <anrijs.avins@gmail.com>
2025-08-15 19:02:36 +0300

    initial commit

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,6 +1,8 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
+
 
 public class NumbersFromAFile {
 
@@ -13,6 +15,21 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        int counter = 0;
+        try (Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                int numbers = Integer.valueOf(row);
+                
+                if (numbers >= lowerBound && numbers <= upperBound) {
+                    counter++;
+                }
+            }
+            System.out.println("Numbers: " + counter);
+            
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex);
+        }
 
     }
 
